08a1a1afa3c83fedf4f6fc8f86ef0daeefb5735f
jagdishwor <jaxthapa449@gmail.com>
2025-01-01 10:22:58 +0545

    list add

---
+++ python_list.py
@@ -0,0 +1,27 @@
+empty_list =[]
+
+list_fruits =["Aplle", "Banana", "Orange"]
+
+print(list_fruits)
+list_fruits[0]= "mango"
+
+print(list_fruits)
+
+list_fruits.append("kiwi")
+
+print(list_fruits)
+
+
+list_fruits.insert(1,"guava")
+
+print(list_fruits)
+
+list_fruits.remove("kiwi")
+
+print(list_fruits)
+
+
+list_fruits=["Apple," "Banana," "Orange"]
+list_fruits.reverse()
+
+print(list_fruits)
